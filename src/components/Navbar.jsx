be42662df7917f98e21cafab3070ba8dde75eea0
Gustavo <75509324+guskpo20@users.noreply.github.com>
2023-05-10 23:51:09 -0300

    Navbar link active first step

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,18 @@
-import { Link } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
 import { useState } from "react"
 import styles from "../styles/navbar.module.css"
 
 function Navbar() {
     const [expandedNavbar, setExpandedNavbar] = useState(false)
     
-
     return (
     <header>
         <div className={styles.navbar}>
             <img className={styles.logo} src={"../../public/images/logo.jpg"}></img>
             <ul className={styles.list}>
-                <li><Link to="/" className={styles.links}>Inicio</Link></li>
-                <li><Link to="/menu" className={styles.links}>Menu</Link></li>
-                <li><Link to="/about" className={styles.links}>Sobre nosotros</Link></li>
+                <li><NavLink to="/" className={({ isActive }) => (isActive ? `active_link ${styles.links}` : `${styles.links}`) }>Inicio</NavLink></li>
+                <li><NavLink to="/menu" className={({ isActive }) => (isActive ? `active_link ${styles.links}` : `${styles.links}`) }>Menu</NavLink></li>
+                <li><NavLink to="/about" className={({ isActive }) => (isActive ? `active_link ${styles.links}` : `${styles.links}`) }>Sobre nosotros</NavLink></li>
             </ul>
             <Link to="/login" className={`${styles.login}`}>Identificarse</Link>
             <div className={expandedNavbar ? `${styles.toggle_btn} ${styles.toggle_btn_open}` : `${styles.toggle_btn}` } onClick={() => setExpandedNavbar(!expandedNavbar)}>
